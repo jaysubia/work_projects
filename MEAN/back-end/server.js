18788d05fee95dafc6d4ecae625bc5f3b38513ba
Jay Subia <jermainesubia@gmail.com>
2017-01-23 20:34:11 -0800

    add mongoose

--- MEAN/back-end/server.js
+++ MEAN/back-end/server.js
@@ -40,6 +40,14 @@ mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     };
 })
 
+app.use(bodyParser.json());
+
+app.post('/api/message',function(req,res){
+    console.log(req.body);
+    res.status(200);
+})
+
+
 var server = app.listen(5000, function(){
     console.log('listening on port', server.address().port);
 })
